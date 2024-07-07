6b2424f57e7c4888ef14ecd4f91769f95d8c88dd
Sara Morote Banos <Sara Morote Banos>
2024-07-07 13:28:58 +0200

    css barra de navegación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { SQLiteConnection } from '@capacitor-community/sqlite';
-import { SQLiteDBConnection } from '@capacitor-community/sqlite';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [ SQLiteConnection, SQLiteDBConnection, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
